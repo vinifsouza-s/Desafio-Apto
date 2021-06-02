abd3717131d9292e6b1bdbc9bfe3365ad7ab1d6d
Vinícius Souza <vinicius.sfs91@gmail.com>
2021-06-02 18:05:57 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import SearchBar from './Components/SearchBar/SearchBar';
+import logo from './logo.png';
+import logo1 from './logo1.png';
+import { BrowserRouter , Switch , Route , Link} from "react-router-dom"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <header className="App-header">
+        <img src={logo1} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo1" alt="logo1" />
+        
       </header>
-    </div>
+      <BrowserRouter>    
+      <Switch>
+      <Route path="/" exact component={Home}/>
+      <Route path="/searchbar" component={SearchBar}/>
+      </Switch>
+      
+      <Link to="/">
+    <button 
+        className="ui primary button"
+        type="submit">
+           Voltar a tela inicial</button>
+    </Link>
+    </BrowserRouter>
+      </div>
+    
   );
 }
+
+const Home = () => (
+  <div>
+    <h1>Pesquisa de Diretorios</h1>
+    <Link to="/searchbar">
+    <button
+      className="ui primary button"
+      type="submit"
+        >Clique aqui para iniciar a pesquisa</button>
+    </Link>
+  </div>
+)
 
 export default App;
---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import DataBar from "../DataBar/DataBar";
+
+
+const SearchBar = () => {
+  const [data, setData] = useState({});
+  const [username, setUsername] = useState("");
+  const [repositories, setRepositories] = useState([]);
+
+  const onChangeHandler = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const submitHandler = async e => {
+    e.preventDefault();
+
+    const profile = await fetch(`https://api.github.com/users/${username}`);
+    const profileJson = await profile.json();
+    // console.log(profileJson);
+
+    const repositories = await fetch(profileJson.repos_url);
+    const repoJson = await repositories.json();
+    console.log(repoJson);
+
+    if (profileJson) {
+      setData(profileJson);
+      setRepositories(repoJson);
+    }
+  };
+  return (
+    <>
+      <div style={{ padding: 20 }}>
+        <div className="ui search">
+          <div className="ui icon input">
+            <i className="search icon"></i>
+            <input
+              className="prompt"
+              placeholder="Digite o nome..."
+              type="text"
+              value={username}
+              onChange={onChangeHandler}
+            />
+          </div>
+            
+          <button
+            className="ui primary button"
+            type="submit"
+            onClick={submitHandler}
+          >
+            <i className="github icon"></i>
+            Search
+          </button>
+
+          
+          
+          <button
+          className="ui primary button"
+          type="submit">Limpar pesquisa</button>
+         
+          
+          <DataBar data={data} repositories={repositories} />
+          
+          
+        </div>
+      </div>
+    </>
+  );
+};
+export default SearchBar;
